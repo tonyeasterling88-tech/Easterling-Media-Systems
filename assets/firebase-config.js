b041c729dd3c045182047114f7524bfd4bbddb14
tonyeasterling88-tech <tonyeasterling88@gmail.com>
2026-04-13 13:22:39 -0400

    Configure Firebase web app settings

--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'replace-me',
-  authDomain: 'replace-me.firebaseapp.com',
-  projectId: 'replace-me',
-  storageBucket: 'replace-me.firebasestorage.app',
-  messagingSenderId: 'replace-me',
-  appId: 'replace-me',
+  apiKey: 'AIzaSyB6LTNJHOxQcapbYE_ymKYnlhyvpRKjJms',
+  authDomain: 'easterling-media-systems.firebaseapp.com',
+  projectId: 'easterling-media-systems',
+  storageBucket: 'easterling-media-systems.firebasestorage.app',
+  messagingSenderId: '822728546713',
+  appId: '1:822728546713:web:486bb1e06e03d0cb069b08',
+  measurementId: 'G-H69HP14J8Z',
 };
